a6538a456153dea69162e8b1a8ebb1d33a95bd90
Liana Baryshova <barishovaliana@gmail.com>
2021-08-26 14:52:39 +0200

    Add <FriendList> component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Profile from './components/Profile/Profile';
 import Statistics from './components/Statistics/Statistics';
-import statisticalData from './statistical-data.json';
-import user from './user.json';
+import FriendList from './components/FriendList/FriendList';
+import user from './components/Profile/user.json';
+import statisticalData from './components/Statistics/statistical-data.json';
+import friends from './components/FriendList/friends.json';
 
 export default function App() {
   return (
@@ -15,6 +17,7 @@ export default function App() {
       />
       <Statistics title="Upload stats" stats={statisticalData} />;
       <Statistics stats={statisticalData} />;
+      <FriendList friends={friends} />,
     </div>
   );
 }
