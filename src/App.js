7c75a4f40a44b9f5dd8154cf730f299288fe19f4
Liana Baryshova <barishovaliana@gmail.com>
2021-08-26 22:11:20 +0200

    Add component <TransactionHistory>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Profile from './components/Profile/Profile';
 import Statistics from './components/Statistics/Statistics';
 import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+
 import user from './components/Profile/user.json';
 import statisticalData from './components/Statistics/statistical-data.json';
 import friends from './components/FriendList/friends.json';
+import transactions from './components/TransactionHistory/transactions.json';
 
 export default function App() {
   return (
@@ -17,7 +20,8 @@ export default function App() {
       />
       <Statistics title="Upload stats" stats={statisticalData} />;
       <Statistics stats={statisticalData} />;
-      <FriendList friends={friends} />,
+      <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
     </div>
   );
 }
